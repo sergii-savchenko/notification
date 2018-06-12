5547ee6a3a96c982073b2e9ba9022ec8d06d3cfe
Sergii Savchenko <sergii.savchenko@me.com>
2018-06-12 04:35:45 +0300

    update

--- pub/pub.js
+++ pub/pub.js
@@ -3,16 +3,21 @@ var client  = mqtt.connect('mqtt://emq')
 
 const uuidv4 = require('uuid/v4');
 
+
 client.on('connect', function () {
-    while (true) {
-      client.publish('loadtest', JSON.stringify({
-        uuid: uuidv4(),
-        payload: {
-          high: 1,
-          low: 0,
-          open: 0.4,
-          close: 0.5,
-          volume: 111111111
-        }}) )
-    }
+    
+  (function loop() {
+      return delay(0).then(function() {
+        client.publish('loadtest', JSON.stringify({
+          uuid: uuidv4(),
+          payload: {
+            high: 1,
+            low: 0,
+            open: 0.4,
+            close: 0.5,
+            volume: 111111111
+          }})
+      }).then(loop);
+  })()
+    
 })
